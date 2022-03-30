cbb02bed89ee8ccb36d419a81a044c4d68e8e509
poludnev <poludnev.roman@gmail.com>
2022-03-30 12:47:50 +0300

    de-hardcoded routes

--- src/SERVER/routers/index.ts
+++ src/SERVER/routers/index.ts
@@ -1,9 +1,7 @@
 import apiRouter from './apiRouter.js';
-import indexRouter from './indexRouter.js';
 import redirect from './redirect.js';
 
 export default {
   apiRouter,
-  indexRouter,
   redirect
 };
