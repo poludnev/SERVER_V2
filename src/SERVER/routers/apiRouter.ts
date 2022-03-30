dbb7b901fe380a24f8f8b0f096219c8577165900
poludnev <poludnev.roman@gmail.com>
2022-03-30 13:22:32 +0300

    refactored controllers imports

--- src/SERVER/routers/apiRouter.ts
+++ src/SERVER/routers/apiRouter.ts
@@ -1,76 +1,67 @@
 import express from 'express';
 
-import {
-  getMoney,
-  getExpenses,
-  getIncomes,
-  getBalances,
-  updateMoney,
-  updateBalance,
-  addExpense,
-  addIncome,
-  addBalance,
-  deleteExpenses,
-  deleteIncomes,
-  deleteBalance
-} from '../controllers/moneyController.js';
-import {
-  addMoneyUser,
-  getMoneyUsers,
-  deleteMoneyUser,
-  getMoneyUsersAll
-} from '../controllers/userController.js';
-import {
-  getBody,
-  getTraining,
-  updateBody,
-  updateTraining,
-  addBody,
-  addTraining,
-  deleteBody,
-  deleteTraining
-} from '../controllers/healthController.js';
+import moneyController from '../controllers/moneyController.js';
+import usersController from '../controllers/usersController.js';
+import healthController from '../controllers/healthController.js';
 
 import routes from '../lib/routes/apiRoutes.js';
 
 const apiRouter = express.Router();
 
-apiRouter.route(routes.addUserRoute()).post(addMoneyUser);
-apiRouter.route(routes.getUsersRoute()).get(getMoneyUsers);
-apiRouter.route(routes.getUsersAllRoute()).get(getMoneyUsersAll);
-apiRouter.route(routes.getUserByLoginRoute()).get(getMoneyUsers);
-apiRouter.route(routes.deleteUserRoute()).post(deleteMoneyUser);
+apiRouter.route(routes.addUserRoute()).post(usersController.addMoneyUser);
+apiRouter.route(routes.getUsersRoute()).get(usersController.getMoneyUsers);
+apiRouter
+  .route(routes.getUsersAllRoute())
+  .get(usersController.getMoneyUsersAll);
+apiRouter
+  .route(routes.getUserByLoginRoute())
+  .get(usersController.getMoneyUsers);
+apiRouter.route(routes.deleteUserRoute()).post(usersController.deleteMoneyUser);
 
-apiRouter.route(routes.getMoneyByIdRoute()).get(getMoney);
-apiRouter.route(routes.getMoneyAllRoute()).get(getMoney);
-apiRouter.route(routes.getExpensesRoute()).get(getExpenses);
-apiRouter.route(routes.getIncomeRoute()).get(getIncomes);
-apiRouter.route(routes.getBalancesRoute()).get(getBalances);
-apiRouter.route(routes.getBalancesByIdRoute()).get(getBalances);
+apiRouter.route(routes.getMoneyByIdRoute()).get(moneyController.getMoney);
+apiRouter.route(routes.getMoneyAllRoute()).get(moneyController.getMoney);
+apiRouter.route(routes.getExpensesRoute()).get(moneyController.getExpenses);
+apiRouter.route(routes.getIncomeRoute()).get(moneyController.getIncomes);
+apiRouter.route(routes.getBalancesRoute()).get(moneyController.getBalances);
+apiRouter.route(routes.getBalancesByIdRoute()).get(moneyController.getBalances);
 
-apiRouter.route(routes.updateMoneyByIdRoute()).post(updateMoney);
-apiRouter.route(routes.updateBalanceByIdRoute()).post(updateBalance);
+apiRouter
+  .route(routes.updateMoneyByIdRoute())
+  .post(moneyController.updateMoney);
+apiRouter
+  .route(routes.updateBalanceByIdRoute())
+  .post(moneyController.updateBalance);
 
-apiRouter.route(routes.addExpenseRoute()).post(addExpense);
-apiRouter.route(routes.addIncomeRoute()).post(addIncome);
-apiRouter.route(routes.addBalanceRoute()).post(addBalance);
+apiRouter.route(routes.addExpenseRoute()).post(moneyController.addExpense);
+apiRouter.route(routes.addIncomeRoute()).post(moneyController.addIncome);
+apiRouter.route(routes.addBalanceRoute()).post(moneyController.addBalance);
 
-apiRouter.route(routes.deleteExpenseRoute()).post(deleteExpenses);
-apiRouter.route(routes.deleteIncomeRoute()).post(deleteIncomes);
-apiRouter.route(routes.deleteBalanceRoute()).post(deleteBalance);
+apiRouter
+  .route(routes.deleteExpenseRoute())
+  .post(moneyController.deleteExpenses);
+apiRouter.route(routes.deleteIncomeRoute()).post(moneyController.deleteIncomes);
+apiRouter
+  .route(routes.deleteBalanceRoute())
+  .post(moneyController.deleteBalance);
 
-apiRouter.route(routes.getBodyRoute()).get(getBody);
-apiRouter.route(routes.getBodyByIdRoute()).get(getBody);
-apiRouter.route(routes.getTrainingRoute()).get(getTraining);
-apiRouter.route(routes.getTrainingByIdRoute()).get(getTraining);
+apiRouter.route(routes.getBodyRoute()).get(healthController.getBody);
+apiRouter.route(routes.getBodyByIdRoute()).get(healthController.getBody);
+apiRouter.route(routes.getTrainingRoute()).get(healthController.getTraining);
+apiRouter
+  .route(routes.getTrainingByIdRoute())
+  .get(healthController.getTraining);
 
-apiRouter.route(routes.updateBodyRoute()).post(updateBody);
-apiRouter.route(routes.updateTrainingRoute()).post(updateTraining);
+apiRouter.route(routes.updateBodyRoute()).post(healthController.updateBody);
+apiRouter
+  .route(routes.updateTrainingRoute())
+  .post(healthController.updateTraining);
 
-apiRouter.route(routes.addBodyRoute()).post(addBody);
-apiRouter.route(routes.addTrainingRoute()).post(addTraining);
+apiRouter.route(routes.addBodyRoute()).post(healthController.addBody);
+apiRouter.route(routes.addTrainingRoute()).post(healthController.addTraining);
 
-apiRouter.route(routes.deleteBodyRoute()).post(deleteBody);
-apiRouter.route(routes.deleteTrainingRoute()).post(deleteTraining);
+apiRouter.route(routes.deleteBodyRoute()).post(healthController.deleteBody);
+apiRouter
+  .route(routes.deleteTrainingRoute())
+  .post(healthController.deleteTraining);
 
 export default apiRouter;
