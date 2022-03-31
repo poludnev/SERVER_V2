627665228a8c7b753936eba723776e722d947445
poludnev <poludnev.roman@gmail.com>
2022-03-31 14:21:54 +0300

    add collections path to lib

---
+++ src/SERVER/lib/collections/collectionsPath.ts
@@ -0,0 +1,17 @@
+export default {
+  getMoneyCollectionPath(): string {
+    return 'money';
+  },
+  getUsersCollectionPath(): string {
+    return 'users-test';
+  },
+  getHealthCollectionPath(): string {
+    return 'health';
+  },
+  getBodyCollectionPath(): string {
+    return 'body-test';
+  },
+  trainingCollectionPath(): string {
+    return 'training-test';
+  }
+};
